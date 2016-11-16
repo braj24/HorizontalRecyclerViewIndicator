67bf766b415d1f2d76dbd64b6a6ec66c87da963e
krunal <krunal.multidots@gmail.com>
2016-11-16 19:17:41 +0530

    Image Drawable Added

--- app/src/main/java/com/krunalkapadiya/horizontalrecyclerviewindicator/HorizontalRVAdapter.java
+++ app/src/main/java/com/krunalkapadiya/horizontalrecyclerviewindicator/HorizontalRVAdapter.java
@@ -4,7 +4,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
 
 import java.util.ArrayList;
 
@@ -30,6 +34,18 @@ public class HorizontalRVAdapter extends RecyclerView.Adapter<HorizontalRVAdapte
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         holder.text.setText(mArrayList.get(position));
+
+        ColorGenerator generator = ColorGenerator.MATERIAL;
+        TextDrawable drawable = TextDrawable.builder()
+                .buildRound(String.valueOf(mArrayList.get(position).charAt(0)), generator.getRandomColor());
+        holder.image.setImageDrawable(drawable);
+
+        //TODO When position comes between 4 to arraylist.size() get Notified
+
+        if (position > 4)
+            mActivity.updateIndicator(position);
+
+
     }
 
     @Override
@@ -39,10 +55,12 @@ public class HorizontalRVAdapter extends RecyclerView.Adapter<HorizontalRVAdapte
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public TextView text;
+        public ImageView image;
 
         public ViewHolder(View itemView) {
             super(itemView);
             text = (TextView) itemView.findViewById(R.id.txt_row_text);
+            image = (ImageView) itemView.findViewById(R.id.img_row_image);
         }
     }
 }
--- app/src/main/java/com/krunalkapadiya/horizontalrecyclerviewindicator/MainActivity.java
+++ app/src/main/java/com/krunalkapadiya/horizontalrecyclerviewindicator/MainActivity.java
@@ -1,9 +1,13 @@
 package com.krunalkapadiya.horizontalrecyclerviewindicator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 
@@ -14,14 +18,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
         LinearLayoutManager manager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         recyclerView.setLayoutManager(manager);
-
         addArrayList();
-        HorizontalRVAdapter adapter = new HorizontalRVAdapter(MainActivity.this,mArrayList);
+
+
+        HorizontalRVAdapter adapter = new HorizontalRVAdapter(MainActivity.this, mArrayList);
         recyclerView.setAdapter(adapter);
 
+
+        recyclerView.setOnScrollChangeListener(new View.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(View view, int i, int i1, int i2, int i3) {
+//                int position = recyclerView.getLayoutManager().getPosition(recyclerView);
+                Log.d("RecyclerPostition", String.valueOf(i));
+                Log.d("RecyclerPostition", String.valueOf(i2));
+                Log.d("RecyclerPostition", String.valueOf(i3));
+
+            }
+        });
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                Log.d("RecyclerPnewstate", String.valueOf(newState));
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (dy < 0) {
+
+                    // Recycle view scrolling up...
+                    Log.d("RecyclerPscroll", String.valueOf(dx));
+
+                } else if (dy > 0) {
+                    // Recycle view scrolling down...
+                    Log.d("RecyclerPscroll", String.valueOf(dy));
+                }
+            }
+        });
     }
 
     private void addArrayList() {
@@ -34,5 +72,23 @@ public class MainActivity extends AppCompatActivity {
         mArrayList.add("Dr Strange");
         mArrayList.add("Hulk");
         mArrayList.add("Thor");
+        mArrayList.add("Thor");
+    }
+
+    public void updateIndicator(int selectedPos) {
+
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.pager_indicator_container);
+        linearLayout.removeAllViews();
+        for (int i = 0; i < mArrayList.size() / 4; i++) {
+            ImageView dot = new ImageView(this);
+            if (i == selectedPos / 4) {
+                dot.setImageResource(R.drawable.view_pager_indicator);
+            } else {
+                dot.setImageResource(R.drawable.view_pager_indicator_unselected);
+            }
+            dot.setPadding(0, 0, 30, 0);
+            linearLayout.addView(dot);
+        }
     }
 }
